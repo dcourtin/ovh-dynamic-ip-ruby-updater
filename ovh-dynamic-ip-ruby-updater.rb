895494fd806aa6eb61dddf64d964813a934cf7ce
davicfs1 <dcourtin@users.noreply.github.com>
2021-10-28 17:51:22 +0200

    ovh-dynamic-ip-ruby-updater.rb
    Fetch your public IP address and update an OVH dynamic domain

---
+++ ovh-dynamic-ip-ruby-updater.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+require 'uri'
+require 'net/http'
+
+ovhid     = 'your_nic_given_by_ovh'
+pass      = 'your_ovh_password'
+dyndomain = 'your_domain_to_update'
+
+#get the ip
+uri = URI('https://ident.me')
+res = Net::HTTP.get_response(uri)
+puts res.body if res.is_a?(Net::HTTPSuccess)
+
+#update ovh 
+req_ovh = 'https://'+ovhid+':'+pass+'@www.ovh.com/nic/update?system=dyndns&hostname='+dyndomain+'&myip='+ip
+uri_ovh = URI(req_ovh)
+res_ovh = Net::HTTP.get_response(uri_ovh)
