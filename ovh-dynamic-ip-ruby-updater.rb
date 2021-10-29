edcf95506ef74154a34b9f0e5c5b9e173ca888ec
davicfs1 <dcourtin@users.noreply.github.com>
2021-10-29 08:16:47 +0200

    Update ovh-dynamic-ip-ruby-updater.rb
    Review string concatenation , miscellaneous fixes

--- ovh-dynamic-ip-ruby-updater.rb
+++ ovh-dynamic-ip-ruby-updater.rb
@@ -9,9 +9,10 @@ dyndomain = 'your_domain_to_update'
 #get the ip
 uri = URI('https://ident.me')
 res = Net::HTTP.get_response(uri)
-puts res.body if res.is_a?(Net::HTTPSuccess)
+ip  = res.body if res.is_a?(Net::HTTPSuccess)
 
 #update ovh 
-req_ovh = 'https://'+ovhid+':'+pass+'@www.ovh.com/nic/update?system=dyndns&hostname='+dyndomain+'&myip='+ip
+req_ovh = 'https://#{ovhid}:#{pass}@www.ovh.com/nic/update?system=dyndns&hostname=#{dyndomain}&myip=#{ip}'
 uri_ovh = URI(req_ovh)
 res_ovh = Net::HTTP.get_response(uri_ovh)
+puts res_ovh.body if res_ovh.is_a?(Net::HTTPSuccess)
